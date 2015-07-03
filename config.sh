68fb5baf1ba6e8e65e12db8d808bb1472682045f
Bret Truchan <clone45@gmail.com>
2015-07-03 11:30:20 -0700

    Moved config variables into config.sh

---
+++ config.sh
@@ -0,0 +1,17 @@
+# The GITHUB_TOKEN is unfortunately necessary for
+# some PHP composer library installations that pull form
+# GitHub.  In order to generate this token, you'll need a
+# GitHub account.  The easiest way to generate a token
+# is by logging into GitHub, then navigating to 
+# https://github.com/settings/tokens
+
+GITHUB_TOKEN=(your token here)
+
+BIND_ADDRESS=192.168.50.7
+
+#
+# These aren't used yet, but are printed out one the site has been created
+#
+
+BITBUCKET_LOGIN=
+BITBUCKET_PASSWORD=
--- provision.sh
+++ provision.sh
@@ -1,22 +1,51 @@
-if [ ! $# -eq 2 ]
+# ------------------------------------------------------------------------------
+#  provision.sh
+# ------------------------------------------------------------------------------
+#
+# Usage:
+#
+# provision.sh [sitename]
+#
+# Good example values for SITE:
+#
+#   postalmonkey
+#   instacash
+#   phustr
+#   pigglybonk
+#
+# This script was created for my own use to automate the provisioning of a local 
+# Vagrant development environment for website development using the Laravel 
+# October CMS.  It's intended to be run on Windows.
+#
+# What it does:
+#
+#   Installs and configures Apache
+#   Installs PHP5
+#   Installs MySQL
+#   Installs Git
+#   Installs Composer
+#   Installs October
+#   Installs Grunt
+#   Adds the project to Bitbucket
+#
+# Before running, set variables in config.sh
+#
+
+if [ ! $# -eq 1 ]
   then
-    echo "usage: provision.sh [sitename] [github_oauth_token] [bind-address (defaults to 192.168.50.7)]"
+    echo "usage: provision.sh [sitename]"
     exit 0
 fi
 
+# Load configuration
 SITE=$1
-GITHUB_TOKEN=$2
-BIND_ADDRESS=192.168.50.7
+source config.sh
 
-if [ $# -eq 3 ]
-  then
-    BIND_ADDRESS=$3
-fi
-
+# Find the current working directory
 DIR=`pwd`
 DIR=$(echo "$DIR" | sed 's/^\///' | sed 's/^./\0:/')
-echo $DIR
 
+# Remove any old files
 if [ -f Vagrantfile ]; then
   rm Vagrantfile
 fi
@@ -25,6 +54,7 @@ if [ -f bootstrap.sh ]; then
   rm bootstrap.sh
 fi
 
+# Create the vagrant file and inject the configuration variables
 cat > Vagrantfile << EOF
 Vagrant.configure("2") do |config|
   config.vm.box = "hashicorp/precise32"
@@ -34,7 +64,10 @@ Vagrant.configure("2") do |config|
 end
 EOF
 
-sed "s/\$site/$SITE/g;s/\$github_token/$GITHUB_TOKEN/g;s/\$bind_address/$BIND_ADDRESS/g" <bootstrap.prototype >bootstrap.sh
+sed "s/\$site/$SITE/g;s/\$github_token/$GITHUB_TOKEN/g;
+     s/\$bind_address/$BIND_ADDRESS/g
+     s/\$bitbucket_login/$BITBUCKET_LOGIN/g
+     s/\$bitbucket_password/$BITBUCKET_PASSWORD/g" <bootstrap.prototype >bootstrap.sh
 
 if [ ! -d "www" ]; then
   mkdir www
